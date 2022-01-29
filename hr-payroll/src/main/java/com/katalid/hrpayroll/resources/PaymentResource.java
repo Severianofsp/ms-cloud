680fa2cf28b0c01d53d0c825b5e93f3c5122f37f
Felipe <felipe.kanner@gmail.com>
2022-01-29 15:30:35 -0300

    Hystrix fallback

--- hr-payroll/src/main/java/com/katalid/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/katalid/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.katalid.hrpayroll.resources;
 
 import com.katalid.hrpayroll.entities.Payment;
 import com.katalid.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +17,18 @@ public class PaymentResource {
     @Autowired
     private PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "geetPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         return ResponseEntity
                 .ok()
                 .body(paymentService.getPayment(workerId, days));
+    }
+
+    public ResponseEntity<Payment> geetPaymentAlternative(Long workerId, Integer days) {
+        Payment payment = new Payment("Brann",400.00,days);
+        return ResponseEntity
+                .ok()
+                .body(payment);
     }
 }
